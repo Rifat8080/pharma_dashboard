b7abec7f030ee9d57ef0853065b5cb29b2b75d53
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-27 16:56:58 +0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :bills
+  resources :customers
+  resources :doctors
+  resources :inventories
+  resources :logs
+  resources :notifications
+  resources :order_items
+  resources :orders
+  resources :prescriptions
+  resources :sale_items
+  resources :sales
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # You can add additional routes or custom routes here
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "sales#index" # Assuming you have a home controller with an index action
 end
